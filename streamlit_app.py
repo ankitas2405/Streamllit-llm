54304d6358c87e331ea2b4e34d075fbb4f6f6e1c
ankitas2405 <143781509+ankitas2405@users.noreply.github.com>
2023-09-07 15:49:10 +0530

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ from langchain.llms import OpenAI
 
 st.title('🦜🔗 Quickstart App')
 
-openai_api_key = st.sidebar.text_input('OpenAI API Key')
+openai_api_key = st.sidebar.text_input('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')
 
 def generate_response(input_text):
   llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
